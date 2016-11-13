dfdffc7debc977460b77a3edf86560734ae1b19a
M. MUHSIN <munashif.muhsin@gmail.com>
2016-11-13 16:58:46 +0530

    added save to mongoDB

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ var mongoose = require('mongoose');
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/scripts'));
 
+
+
 mongoose.connect("mongodb://127.0.0.1:27017");
 
 app.get('/', function(req, res){
@@ -25,22 +27,56 @@ var ChatSchema = mongoose.Schema({
 //chat model
 var Chat = mongoose.model('Chat', ChatSchema);
 
-//access control, CORS
+
+//access control
 app.all('*', function(req, res) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,POST');
 });
 
 
+
+
 io.on('connection', function(socket){
   console.log('a user connected');
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+
+  socket.on('join room', function(data) {
+    socket.join(data.room);
+    socket.broadcast.to(data.room).emit('user joined', data);
+    socket.username = data.name;
+    socket.room = data.room;
   });
+
+  socket.on('chat message', function(data){
+     var newMsg = new Chat({
+        created: data.date,
+        message: data.text,
+        sentBy: data.sentBy,
+        room: data.room
+      });
+      //Save it to database
+     newMsg.save(function(err, msg){
+        //Send message to those connected in the room
+        console.log("message recieved: "+ JSON.stringify(data));
+        socket.broadcast.to(msg.room).emit('chat message', data);
+     });
+
+  });
+
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    var data = {
+        left: socket.username
+    }
+    socket.to(socket.room).emit('user left', data);
+    console.log("user left: "+socket.username);
   });
 });
+
+
+
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
--- scripts/angularBackend.js
+++ scripts/angularBackend.js
@@ -1,7 +1,6 @@
-
-
 var myApp = angular.module('chatAPP', ['ngRoute']);
 
+//Route config
 myApp.config(function($routeProvider) {
         $routeProvider
 
@@ -11,7 +10,7 @@ myApp.config(function($routeProvider) {
                 controller  : 'loginCtrl'
             })
 
-            // route for the contact page
+            // route for the chat page
             .when('/room', {
                 templateUrl : 'room.html',
                 controller  : 'roomCtrl'
@@ -19,22 +18,110 @@ myApp.config(function($routeProvider) {
     });
 
 //Login Page Controller
-myApp.controller('loginCtrl', ['$scope','$location', function ($scope,$location) {
+myApp.controller('loginCtrl', ['$scope','$location','loginService', function ($scope,$location,loginService) {
     console.log("Hello World from controller");
     $scope.login = function(data){
-        console.log("name: "+data.username);
-        console.log("room: "+data.room);
+        loginService.update(data.username,data.room);
         $location.path('/room');
     }
 
 }]);
 
 //Chat Page Controller
-myApp.controller('roomCtrl', ['$scope', function ($scope) {
+myApp.controller('roomCtrl', ['$scope','loginService','$location','$anchorScroll', function ($scope,loginService,$location,$anchorScroll) {
     console.log("Hello World from controller");
-    $scope.login = function(data){
-        console.log("name: "+data.username);
-        console.log("room: "+data.room);
+    loginService.init();
+    var data = loginService.get();
+
+    $scope.chatText=[];
+    console.log("name: "+data.name);
+    console.log("room: "+data.room);
+
+
+
+    socket.on('user joined', function(msg){
+      console.log("joined room: "+ JSON.stringify(msg));
+      $scope.chatText.push(msg);
+      $scope.$apply();
+      $("#chatDiv").scrollTop($("#chatDiv").children().height());
+
+    });
+
+    socket.on('user left', function(msg){
+      console.log("left room: "+ JSON.stringify(msg));
+      $scope.chatText.push(msg);
+      $scope.$apply();
+      $("#chatDiv").scrollTop($("#chatDiv").children().height());
+
+    });
+
+    socket.on('chat message', function(msg){
+      console.log("message recieved: "+ JSON.stringify(msg));
+      $scope.chatText.push(msg);
+      $scope.$apply();
+      $("#chatDiv").scrollTop($("#chatDiv").children().height());
+
+    });
+
+
+
+    $scope.sendMessage = function(message){
+        if($scope.txt != ""){
+            var messageObj={
+                sentBy:data.name,
+                room:data.room,
+                text:message,
+                date:Date()
+            };
+            socket.emit('chat message', messageObj);
+            $scope.txt = "";
+            messageObj.sentBy = "me";
+            $scope.chatText.push(messageObj);
+            //$("#chatDiv").scrollTop($("#chatDiv").children().height());
+            $('#chatDiv').animate({"scrollTop": $('#chatDiv')[0].scrollHeight}, "fast"); 
+
+
+        }
+
     }
 
+    socket.emit("join room", data);
+
+}]);
+
+//loginService
+myApp.factory('loginService', function () {
+
+    var data = {};
+
+    return {
+        get: function () {
+            return data
+        },
+        update: function (name,room) {
+            data.name = name;
+            data.room = room;
+            sessionStorage["loginService"] = JSON.stringify(data);
+        },
+
+        init: function () {
+            var temp = JSON.parse(sessionStorage["loginService"]);
+            data.name = temp.name;
+            data.room = temp.room;
+        }
+    };
+});
+
+//socket service
+myApp.factory('socket', ['$rootScope', function($rootScope) {
+  var socket = io.connect();
+
+  return {
+    on: function(eventName, callback){
+      socket.on(eventName, callback);
+    },
+    emit: function(eventName, data) {
+      socket.emit(eventName, data);
+    }
+  };
 }]);
